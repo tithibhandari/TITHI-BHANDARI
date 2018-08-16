1c8210be52cfeedf3f6a86d9238c0c0373e68c0e
tithibhandari <42140486+tithibhandari@users.noreply.github.com>
2018-08-16 21:45:04 +0530

    Add files via upload

---
+++ lab3.cpp
@@ -0,0 +1,29 @@
+// Library
+#include <iostream>
+using namespace std;
+//use mainfunction
+
+int main(){
+//put in an intro
+cout <<"Hello"<<end;
+//Declarethe diffrent types of variables
+//assign values to the variables and define the values
+int a=3;
+float b=3.1;
+char c='t';
+dobule dobulevar=2.12;
+boolboolvar=true;
+//Display the size of the variables to print out the size of variables
+cout<<"size of a="<<sizeof(a)<<endl;
+cout<<" size of b="<<sizeof(b)<<endl;
+cout<<"size of c="<<sizeof(c)<<endl;
+cout<<" size of dobulevar="sizeof(dobulevar)<<endl;
+cout<<" size of boolvar="sizeof(boolvar)<<endl;
+// end the programme
+return 0;
+} 
+ 
+
+
+
+
