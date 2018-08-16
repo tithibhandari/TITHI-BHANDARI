f0cf692e75a518d0ca4fb48b8dc07fa2b2da4a90
tithibhandari <42140486+tithibhandari@users.noreply.github.com>
2018-08-16 21:45:37 +0530

    Add files via upload

---
+++ lab3-q2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main() {
+int a;
+a=5;
+int b;
+b=4
+
+//define oprtaters
+int d=a+b;
+int s=a-b;
+int r=a*b;
+int t=a/b;
+int l=a%b;
+
+//print output
+
+cout<<"The sum of a and b is "<<d<<endl;
+cout<<"the difference of a and d is "<<s<<endl;
+cout<<"the product of a and b is "<<r<<endl;
+cout<<"the quotient of a and b is "<<t<<endl;
+cout<<"the remainder of a and b is "<<l<<endl;
+return 1
+}
+
+ 
+
