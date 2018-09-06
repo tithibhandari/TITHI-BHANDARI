f2398d252d2f8daef93c197bcf270687fe909267
tithibhandari <42140486+tithibhandari@users.noreply.github.com>
2018-09-06 12:18:16 -0400

    Add files via upload

---
+++ lab5.5_q1.cpp
@@ -0,0 +1,24 @@
+//PRINT THE SQUARE STAR PATTERN
+//define library
+#include<iostream>
+using namespace std;
+
+//define the main function
+int main(){
+int n=5;
+int i,j;
+for (int i=0;i< n; i++)
+{
+for (int j=0;j< n;j++)
+//print some stars
+cout<<"*";
+cout<<endl;
+}
+return 0;
+}
+
+
+
+
+
+
